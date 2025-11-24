61107925db2f86e04e2f4d8c7f47bca2b303a1dd
Divyanshu <divyanshusomasekhar1@gmail.com>
2025-11-24 00:22:22 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/T.S.A-website/",   // ← important
   plugins: [react()],
+  base: "/T.S.A-website/",
   server: {
     port: 5173,
     proxy: {
       "/api": {
         target: "http://localhost:3001",
-        changeOrigin: true,
-      },
-    },
-  },
+        changeOrigin: true
+      }
+    }
+  }
 });
