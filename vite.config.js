9bd8c626ad18f271b352031548cc1ea04acde4ba
Divyanshu <divyanshusomasekhar1@gmail.com>
2025-11-24 00:15:51 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/T.S.A-website/",   // ← important
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:3001", // your Express server
+        target: "http://localhost:3001",
         changeOrigin: true,
       },
     },
